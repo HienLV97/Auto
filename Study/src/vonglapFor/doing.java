73b4bf7e6963b1a684f3ba61b81aeab877f9dbe8
levinhhien <levinhhien@localhost>
2022-09-18 20:03:49 +0700

    sssd

--- Study/src/vonglapFor/doing.java
+++ Study/src/vonglapFor/doing.java
@@ -4,6 +4,8 @@ public class doing {
 	public static void main(String[] args) {
 		String a = "hi";
 		System.out.println("ascbasdvc");
-		System.out.println("Done test hithub");
+		System.out.println("Done test hithub");	
+		System.out.println(ye);
 	}
+
 }
